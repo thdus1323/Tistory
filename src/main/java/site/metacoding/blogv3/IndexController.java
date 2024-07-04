bc0b0c3322ebe3041ca2966506dfadf25e3b8684
thdus1323 <thdus1323@naver.com>
2024-07-04 12:50:08 +0900

    뷰 구현

--- src/main/java/site/metacoding/blogv3/IndexController.java
+++ src/main/java/site/metacoding/blogv3/IndexController.java
@@ -13,5 +13,61 @@ public class IndexController {
         return "main";
     }
 
+    //category view----------------------------------------------
+
+    @GetMapping("ca-write-form")
+    public String caWriteForm() {
+        return "/category/writeForm";
+    }
+
+
+    //post view----------------------------------------------
+
+    @GetMapping("detail")
+    public String detail() {
+        return "/post/detail";
+    }
+
+    @GetMapping("list")
+    public String list() {
+        return "/post/list";
+    }
+
+    @GetMapping("po-write-form")
+    public String poWriteForm() {
+        return "/post/writeForm";
+    }
+
+
+
+    //user view----------------------------------------------
+
+    @GetMapping("join-form")
+    public String joinForm() {
+        return "/user/joinForm";
+    }
+
+    @GetMapping("login-form")
+    public String loginForm() {
+        return "/user/loginForm";
+    }
+
+    @GetMapping("password-reset-form")
+    public String passwordResetForm() {
+        return "/user/passwordResetForm";
+    }
+
+    @GetMapping("update-form")
+    public String updateForm() {
+        return "/user/updateForm";
+    }
+
+
+
+
+
+
+
+
 
 }
