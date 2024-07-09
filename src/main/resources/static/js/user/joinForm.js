58478b7ff9db132d534552a1bf4ad64bc1ff5cd4
thdus1323 <thdus1323@naver.com>
2024-07-09 17:30:40 +0900

    값넘기기 중

--- src/main/resources/static/js/user/joinForm.js
+++ src/main/resources/static/js/user/joinForm.js
@@ -35,35 +35,3 @@ function validation() {
         return false;
     }
 }
-
-async function usernameSameCheck() {
-    let username = $("#username").val();
-
-    let response = await fetch(`/api/user/username-same-check?username=${username}`);
-    let responseParse = await response.json();
-
-    if (response.status === 200) {
-        if (!responseParse) {
-            valid.username.state = false;
-            valid.username.msg = "유저네임이 중복되었습니다.";
-        } else {
-            valid.username.state = true;
-            valid.username.msg = "";
-        }
-    } else {
-        alert(responseParse);
-    }
-}
-
-function passwordSameCheck() {
-    let password = $("#password").val();
-    let samePassword = $("#same-password").val();
-
-    if (password === samePassword) {
-        valid.password.state = true;
-        valid.password.msg = "";
-    } else {
-        valid.password.state = false;
-        valid.password.msg = "패스워드가 동일하지 않습니다.";
-    }
-}
